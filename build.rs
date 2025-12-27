436e75e1ceeef6da2607a9cb3ca34a3ec18d875b
Robin Marchart <robin.marchart@gmail.com>
2025-12-27 23:38:02 +0100

    ensure JELLYFIN_TUI_FEATURES is never empty

--- build.rs
+++ build.rs
@@ -1,13 +1,11 @@
 fn main() {
     println!("cargo::rerun-if-env-changed=CARGO_CFG_FEATURE");
     let features = std::env::var("CARGO_CFG_FEATURE").expect("should be set by cargo");
-    let features: Vec<_> = features
-        .split(",")
-        .filter(|s| {
-            let s: &str = s;
-            s != "default"
-        })
-        .collect();
-    let features = features.join(", ");
+    let features: Vec<_> = features.split(",").collect();
+    let features = if features.is_empty() {
+        "∅".to_string()
+    } else {
+        features.join(", ")
+    };
     println!("cargo::rustc-env=JELLYFIN_TUI_FEATURES={features}");
 }
